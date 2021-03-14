ebc5eb4fd1c2e48a1c4621c9676aa3029d62fd70
helihui <helihui_108699@163.com>
2021-03-14 18:49:40 +0800

    computed and del

--- src/store/modules/cart.js
+++ src/store/modules/cart.js
@@ -1,21 +1,67 @@
 const state = {
-  carProducts: []
+  cartProducts: []
 }
-const getters = {}
+const getters = {
+  totalCount (state) {
+    return state.cartProducts.reduce((sum, prod) => sum + prod.count, 0)
+  },
+  totalPrice (state) {
+    return state.cartProducts.reduce((sum, prod) => sum + prod.totalPrice, 0)
+  },
+  checkedCount (state) {
+    return state.cartProducts.reduce((sum, prod) => {
+      if (prod.isChecked) {
+        sum += prod.count
+      }
+      return sum
+    }, 0)
+  },
+  checkedPrice (state) {
+    return state.cartProducts.reduce((sum, prod) => {
+      if (prod.isChecked) {
+        sum += prod.totalPrice
+      }
+      return sum
+    }, 0)
+  }
+}
 const mutations = {
   addToCart (state, product) {
-    const prod = state.carProducts.find(item => item.id === product.id)
+    const prod = state.cartProducts.find(item => item.id === product.id)
     if (prod) {
       prod.count++
       prod.isChecked = true
       prod.totalPrice = prod.count * prod.price
     } else {
-      state.carProducts.push({
+      state.cartProducts.push({
         ...product,
         count: 1,
         isChecked: true,
         totalPrice: product.price
       })
+    }
+  },
+  deleteFromCart (state, prodId) {
+    const index = state.cartProducts.findIndex(item => item.id === prodId)
+    index !== -1 && state.cartProducts.splice(index, 1)
+  },
+  updateAllProductsChecked (state, checked) {
+    state.cartProducts.forEach(prod => {
+      prod.isChecked = checked
+    })
+  },
+  updateProductsChecked (state, { checked, prodId }) {
+    const prod = state.cartProducts.find(prod => prod.id === prodId)
+    prod && (prod.isChecked = checked)
+  },
+  updateProducts (state, {
+    prodId,
+    count
+  }) {
+    const prod = state.cartProducts.find(prod => prod.id === prodId)
+    if (prod) {
+      prod.count = count
+      prod.totalPrice = count * prod.price
     }
   }
 }
